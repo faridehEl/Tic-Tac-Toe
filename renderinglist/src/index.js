07de662f959e909739784dcf6ed7d46280929085
feri <rht.farideh@yahoo.com>
2024-02-12 17:17:36 +0100

    first commit

---
+++ renderinglist/src/index.js
@@ -0,0 +1,11 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import Board from "./Board";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+   <Board />
+  </StrictMode>
+);
