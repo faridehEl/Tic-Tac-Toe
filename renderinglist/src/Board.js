d16eb4ab0a846b7b48fb8b9eb30636a096cdb965
feri <rht.farideh@yahoo.com>
2024-02-24 16:55:06 +0100

    add array for square

--- renderinglist/src/Board.js
+++ renderinglist/src/Board.js
@@ -1,23 +1,25 @@
 import Square from "./Square"
+import { useState } from "react";
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return(
    
     <>
     <div className="Board">
     <div className="board-row">
-    <Square value={1}/>
-    <Square value={2}/>
-    <Square value={3} />
+    <Square value={squares[0]} />
+    <Square value={squares[1]} />
+    <Square value={squares[2]} />
     </div>
     <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+    <Square value={squares[3]} />
+    <Square value={squares[4]} />
+    <Square value={squares[5]} />
     </div>
     <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+    <Square value={squares[6]} />
+    <Square value={squares[7]} />
+    <Square value={squares[8]} />
     </div>
     </div>
     </>
