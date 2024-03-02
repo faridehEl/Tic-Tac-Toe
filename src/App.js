6e5094ba370f7016e03d996eae4f621f3f9aea85
feri <rht.farideh@yahoo.com>
2024-03-02 23:00:29 +0100

    🎉 who is winner

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function Board() {
   if (winner) {
     status = "Winner: " + winner;
   } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    status = "Next player: " + (XisNext ? "X" : "O");
   }
   function handleClick(i){
 
@@ -31,6 +31,7 @@ export default function Board() {
   return(
    
     <>
+    <div className="status">{status}</div>
     <div className="Board">
     <div className="board-row">
     <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
